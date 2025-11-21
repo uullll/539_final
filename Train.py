571a4b43e09cff14b6edcad03cb34a5f4c789fc0
lucasalopz <lucasalopz@gmail.com>
2025-11-21 17:50:23 -0600

    Add files via upload
    Extensive changes to Train. The "best model" is now chosen based on a moving average of a loss ratio and the accuracy of positive samples (pos_acc). Created a seed function for reproducibility. Added new model: an EfficientNetB6 model. There was a typo in the previous Main: I was not using the sampler. Also, positive sample loss is weighted to about 0.4*57. My experiments show that higher values are too aggressive, although I still need to fine tune. We need the 2019 data. I added a patience threshold to train(), although I don't think we'll be using it because after about 2-3 epochs, the model begins overfitting.

--- Train.py
+++ Train.py
@@ -7,39 +7,54 @@ import matplotlib.pyplot as plt
 
 
 def train(net, train_dl, val_dl, criterion, opt, epochs = 3, lr = 1e-4,
-          device = "mps", best_model_name = "best.pt", threshold = 0.5):
+          device = "mps", best_model_name = "best.pt", threshold = 0.5, patience = 2):
+
     # Set training specs
     device = device if torch.backends.mps.is_available() else "cpu"
     scaler = GradScaler(device=device)
     for g in opt.param_groups:
         g["lr"] = lr
 
-    best_auc = 0.0
     use_meta = True if net.meta is not None else False
     hist_train_loss, hist_val_loss, hist_val_acc = [], [], []
+    average_loss_ratio = float("inf")
+    no_improvement = 0
+    best_pos_acc = 0
 
     for ep in range(epochs):
         train_loss, train_acc = train_one_epoch(net, train_dl, scaler, device, criterion, opt, use_meta, threshold)
         ys, ps, val_loss, val_acc = val_one_epoch(net, val_dl, criterion, device, use_meta, threshold)
-        ys = torch.cat(ys).numpy()
-        ps = torch.cat(ps).numpy()
-        auc = roc_auc_score(ys, ps)
+        ys = torch.cat(ys)
+        ps = torch.cat(ps)
+        auc = roc_auc_score(ys.numpy(), ps.numpy())
 
-        print(f"Epoch {ep+1}/{epochs} AUC={auc:.4f}")
+        # Compute positive accuracy
+        preds = (ps >= threshold).float()
+        pos_mask = (ys == 1)
+        pos_acc = (preds[pos_mask] == 1).float().mean().item() if pos_mask.sum() > 0 else 0.0
+
+        # Compute loss ratio
+        loss_ratio = val_loss / train_loss if train_loss > 0 else float("inf")
 
         # Save best model
-        if auc > best_auc:
-            best_auc = auc
+        if pos_acc > best_pos_acc and loss_ratio <= average_loss_ratio:
+            average_loss_ratio = loss_ratio if ep == 0 else (loss_ratio + average_loss_ratio) / 2
+            best_pos_acc = pos_acc
             torch.save(net.state_dict(), best_model_name)
-            print(f"Saved new best model at epoch {ep+1} with AUC={auc:.4f}")
+            no_improvement = 0
+            print(f"Saved new best model at epoch {ep + 1} with AUC={auc:.4f}")
+        else:
+            no_improvement += 1
+            if no_improvement > patience:
+                print("Early Stopping because of no improvement.")
+                break
 
         hist_train_loss.append(train_loss)
         hist_val_loss.append(val_loss)
         hist_val_acc.append(val_acc)
+        print(f"Epoch {ep + 1}: Loss Ratio={loss_ratio:.4f}, Val AUC={auc:.4f}, Val Pos Acc={pos_acc:.4f}")
 
-        print(f"\nEpoch {ep + 1}/{epochs}")
-
-    print(f"Best validation AUC: {best_auc:.4f}")
+    print(f"Best validation pos_acc: {best_pos_acc:.4f}")
     return hist_train_loss, hist_val_loss, hist_val_acc
 
 
@@ -78,7 +93,7 @@ def val_one_epoch(net, val_dl, criterion, device, use_meta, threshold = 0.5):
     # Validate
     net.eval()
     ys, ps = [], []
-    val_loss, val_acc = [], []
+    val_loss, val_acc, pos_acc = [], [], []
     with torch.no_grad():
         for xb, yb, mb in val_dl:
             xb = xb.to(device)
@@ -91,7 +106,7 @@ def val_one_epoch(net, val_dl, criterion, device, use_meta, threshold = 0.5):
             ys.append(yb.cpu())
             ps.append(prob.cpu())
             val_loss.append(loss.detach().item())
-            preds = (torch.sigmoid(logits) >= threshold).float()
+            preds = (prob >= threshold).float()
             val_acc.append((preds == yb).float().mean().item())
 
     return ys, ps, np.mean(val_loss), np.mean(val_acc)
@@ -119,3 +134,26 @@ def plot_train_hist(hist_train_loss, hist_val_loss, hist_val_acc, epochs = 3):
 
     plt.tight_layout()
     plt.show()
+
+
+import random
+import numpy as np
+import torch
+
+
+def set_seed_mps(seed=33):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    if torch.backends.mps.is_available():
+        torch.manual_seed(seed)
+
+
