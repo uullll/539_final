a351ab29fcda76d737995ae47551c929a5703164
lucasalopz <lucasalopz@gmail.com>
2025-11-21 17:50:23 -0600

    Add files via upload
    Extensive changes to Train. The "best model" is now chosen based on a moving average of a loss ratio and the accuracy of positive samples (pos_acc). Created a seed function for reproducibility. Added new model: an EfficientNetB6 model. There was a typo in the previous Main: I was not using the sampler. Also, positive sample loss is weighted to about 0.4*57. My experiments show that higher values are too aggressive, although I still need to fine tune. We need the 2019 data. I added a patience threshold to train(), although I don't think we'll be using it because after about 2-3 epochs, the model begins overfitting.

--- Models.py
+++ Models.py
@@ -30,7 +30,6 @@ class ResNet18Meta(nn.Module):
 
 # In[ ]:
 
-
 class ResNet101Meta(nn.Module):
     def __init__(self, meta_dim=0):
         super().__init__()
@@ -52,3 +51,31 @@ class ResNet101Meta(nn.Module):
             f = torch.cat([f, self.meta(m)], dim=1)
         return self.head(f).squeeze(1)
 
+
+class Efficientnet_b6(nn.Module):
+    def __init__(self, meta_dim=0):
+        super().__init__()
+
+        self.backbone = tv.models.efficientnet_b6(
+            weights=tv.models.EfficientNet_B6_Weights.IMAGENET1K_V1
+        )
+        in_feats = self.backbone.classifier[1].in_features
+        self.backbone.classifier[1] = nn.Identity()
+
+        if meta_dim > 0:
+            self.meta = nn.Sequential(
+                nn.Linear(meta_dim, 32), nn.ReLU(), nn.Dropout(0.1),
+                nn.Linear(32, 16), nn.ReLU()
+            )
+            self.head = nn.Linear(in_feats + 16, 1)
+        else:
+            self.meta = None
+            self.head = nn.Linear(in_feats, 1)
+
+    def forward(self, x, m=None):
+        f = self.backbone(x)
+
+        if self.meta is not None and m is not None and m.numel() > 0:
+            meta_out = self.meta(m)
+            f = torch.cat([f, meta_out], dim=1)
+        return self.head(f).squeeze(1)
